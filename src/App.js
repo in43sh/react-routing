79ca2aed476c7aed5fb28f46a0941527b6282476
Sergey Sherstobitov <in43sh@gmail.com>
2017-12-28 17:49:21 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import routes from './routes';
+import { Switch, Link } from 'react-router-dom';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+
+        <Link to="/">
+          <p>Route1</p>
+        </Link>
+
+        <Link to="/2">
+          <p>Route3</p>
+        </Link>
+        
+        <Link to="/3">
+          <p>Route3</p>
+        </Link>
+
+        { routes }
+      </div>
+    );
+  }
+}
+
+export default App;
